a92733d73ab14c5a2e4be9c6679c1964c3560a34
Jacob Rose <jacob@rostor.cloud>
2025-06-10 18:01:14 +0200

    added code to draw a line

--- graphics.py
+++ graphics.py
@@ -1,13 +1,34 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, start: Point, end: Point):
+        self.start = start
+        self.end = end
+
+    def draw(self, canvas, fill_color: str):
+        canvas.create_line(
+            self.start.x,
+            self.start.y,
+            self.end.x,
+            self.end.y,
+            fill=fill_color,
+            width=2
+        )
+
 class Window():
     def __init__(self, width, height):
        self.__root = Tk()
        self.__root.title("Maze Solver")
+       self.__root.protocol("WM_DELETE_WINDOW", self.close)
        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
        self.__canvas.pack(fill=BOTH, expand=1)
        self.__running = False
-       self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -18,6 +39,9 @@ class Window():
         while self.__running:
             self.redraw()
         print("window closed...")
+
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(self.__canvas, fill_color)
 
     def close(self):
         self.__running = False
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(16, 18), Point(200, 300))
+    win.draw_line(line, "red")
     win.wait_for_close()
 
 if __name__ == '__main__':
