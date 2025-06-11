d80c9d6f2792dccb56e9a298ca1494b2dfdf01df
Jacob Rose <jacob@rostor.cloud>
2025-06-11 17:37:22 +0200

    added the center to center draw function

--- cell.py
+++ cell.py
@@ -13,6 +13,8 @@ class Cell():
         self.__win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self.__win is None:
+            return
         self.__x1 = x1
         self.__x2 = x2
         self.__y1 = y1
@@ -29,3 +31,14 @@ class Cell():
         if self.has_bottom_wall:
             line = Line(Point(x1,y2), Point(x2,y2))
             self.__win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        self_center_x = (self.__x2 + self.__x1) / 2
+        self_center_y = (self.__y2 + self.__y1) / 2
+        to_center_x = (to_cell.__x2 + to_cell.__x1) / 2
+        to_center_y = (to_cell.__y2 + to_cell.__y1) / 2
+        line = Line(Point(self_center_x, self_center_y), Point(to_center_x, to_center_y))
+        if not undo:
+            self.__win.draw_line(line,fill_color="red")
+        else:
+            self.__win.draw_line(line, fill_color="gray")
