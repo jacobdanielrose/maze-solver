64d91e7039989f35b5376a9809bce412c68918c0
Jacob Rose <jacob@rostor.cloud>
2025-06-13 16:24:18 +0200

    opened the entrance and exits of the maze

--- tests.py
+++ tests.py
@@ -32,5 +32,20 @@ class Tests(unittest.TestCase):
             Maze(0, 0, num_rows, num_cols, 10, 10)
 
 
+    def test_maze_break_entrace_and_exit(self):
+        num_cols = 10
+        num_rows = 15
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+
+        self.assertEqual(
+            m1._Maze__cells[0][0].has_top_wall,
+            False,
+        )
+        self.assertEqual(
+            m1._Maze__cells[num_cols-1][num_rows-1].has_bottom_wall,
+            False,
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
