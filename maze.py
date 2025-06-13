03b646f680b1879e8225bf635a798586261e6fcb
Jacob Rose <jacob@rostor.cloud>
2025-06-13 15:23:20 +0200

    slight improvements

--- maze.py
+++ maze.py
@@ -14,32 +14,35 @@ class Maze():
             cell_size_y: Union[int, float],
             win: Window,
         ) -> None:
-        self.x1 = x1
-        self.y1 = y1
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self.cell_size_x = cell_size_x
-        self.cell_size_y = cell_size_y
-        self.win = win
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__win = win
         self.__cells = []
         self.__create_cells()
 
     def __create_cells(self) -> None:
-        for i in range(self.num_cols):
+        for i in range(self.__num_cols):
             self.__cells.append([])
-            for j in range(self.num_rows):
-                self.__cells[i].append(Cell(self.win))
+            for j in range(self.__num_rows):
+                self.__cells[i].append(Cell(self.__win))
                 self.__draw_cell(i,j)
 
     def __draw_cell(self, col: int, row: int) -> None:
-        cell = self.__cells[col][row]
-        x1 = self.cell_size_x * col + self.x1
-        x2 = x1 + self.cell_size_x
-        y1 = self.cell_size_y * row + self.y1
-        y2 = y1 + self.cell_size_y
-        cell.draw(x1, y1, x2, y2)
+        if self.__win is None:
+            return
+        x1 = self.__cell_size_x * col + self.__x1
+        y1 = self.__cell_size_y * row + self.__y1
+        y2 = y1 + self.__cell_size_y
+        x2 = x1 + self.__cell_size_x
+        self.__cells[col][row].draw(x1, y1, x2, y2)
         self.__animate()
 
     def __animate(self) -> None:
-        self.win.redraw()
+        if self.__win is None:
+            return
+        self.__win.redraw()
         sleep(0.0005)
